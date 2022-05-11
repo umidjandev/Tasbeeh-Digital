0c1508cf7ff437171a9a51a1234a256469b194d8
Umidjan <jumabayevdev21@gmail.com>
2022-05-12 00:15:23 +0500

    first com

---
+++ app/src/main/java/com/najdimu/tasbeehdigital/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.najdimu.tasbeehdigital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.najdimu.tasbeehdigital.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    var count = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCounter.setOnClickListener {
+            binding.counterNum.setText(count.toString())
+            count++
+        }
+        binding.btnReset.setOnClickListener {
+            binding.counterNum.setText("0")
+            count=1
+        }
+
+
+    }
+}
