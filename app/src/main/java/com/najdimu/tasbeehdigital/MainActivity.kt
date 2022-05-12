7f55f17e5fec3e22f81d21b8dc3c22f5bee2338d
Umidjan <jumabayevdev21@gmail.com>
2022-05-12 23:26:42 +0500

    tasbeeh nearly done

--- app/src/main/java/com/najdimu/tasbeehdigital/MainActivity.kt
+++ app/src/main/java/com/najdimu/tasbeehdigital/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.najdimu.tasbeehdigital
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.content.res.ResourcesCompat
 import com.najdimu.tasbeehdigital.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     var count = 1
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.btnCounter.setOnClickListener {
             binding.counterNum.setText(count.toString())
